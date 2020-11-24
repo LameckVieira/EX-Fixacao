bdfc9a3adcb9841dbe9ab0736686799c93b6119a
LameckVieira <lameck.v.barbosa@gmail.com>
2020-11-24 20:13:18 -0300

    Ex 1, 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,75 @@ namespace EX_Fixacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Exercício 1");
+
+
+            // Console.WriteLine("Digite o ano atual");
+            // int anoAtual =int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Digite o seu ano de nascimento");
+            // int anoDeNacimento = int.Parse(Console.ReadLine());
+
+
+            // int idade = anoAtual - anoDeNacimento;
+            // Console.WriteLine(idade);
+
+            // if(idade>16){
+            //     Console.WriteLine("Você pode votar!");
+            // }else{
+            //     Console.WriteLine("Você não pode votar ainda!");
+            // }
+
+            // Console.WriteLine("Exercício 2");
+
+            // Console.WriteLine("Selecione um tipo de Combustível");
+            // Console.WriteLine("A - Álcool");
+            // Console.WriteLine("G - Gasolina");
+            // String combustivel = Console.ReadLine();
+
+            // Console.WriteLine("Digite a quantidade de combustível");
+            // float quantidadedeCombustivel = float.Parse(Console.ReadLine());
+            
+            
+            // float percDesconto, desconto;
+            // float precoAlcool = 4.9f;
+            // float precoGasolida = 5.3f;
+            // float valorTotal;
+
+            // switch (combustivel.ToLower())
+            // {
+            //     case "a":
+            //         if(quantidadedeCombustivel>20){
+            //             percDesconto = 0.05f;
+            //         }else{
+            //             percDesconto = 0.03f;
+            //         }
+            //         valorTotal = (quantidadedeCombustivel * precoGasolida);
+            //         desconto = (quantidadedeCombustivel * precoAlcool) * percDesconto;
+            //         Console.WriteLine($"Valor a ser pago{valorTotal-desconto}");
+            //         break;
+            //     case "g":
+            //         if(quantidadedeCombustivel>20){
+            //             percDesconto = 0.05f;
+            //         }else{
+            //             percDesconto = 0.03f;
+            //         }
+            //         valorTotal = (quantidadedeCombustivel * precoGasolida);
+            //         desconto = (quantidadedeCombustivel * precoGasolida) * percDesconto;
+            //         Console.WriteLine($"Valor a ser pago{valorTotal-desconto}");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Opção inválida");
+            //         break;
+            // }
+            Console.WriteLine("Exercício 3");
+
+
+            
+             
+
+
+
         }
     }
 }
